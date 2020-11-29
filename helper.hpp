411433f32267273db7cd7a50f9aac29ac5b34845
leonardung <75213951+leonardung@users.noreply.github.com>
2020-11-29 22:12:56 +0100

    Add files via upload

--- helper.hpp
+++ helper.hpp
@@ -6,16 +6,16 @@
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 
-using namespace cv;
+using namespace std;
 
 
 cv::Mat load_image(std::string image_path);
 
 void display_im(cv::Mat);
 
-std::vector<uchar> cv_to_vector(cv::Mat image);
+std::vector<vector < vector < int>>> cv_to_vector(cv::Mat image);
 
-std::vector<Mat> split_channels(cv::Mat image);
+std::vector<cv::Mat> split_channels(cv::Mat image);
 
 std::vector<std::vector<uchar>> channels_to_vectors(cv::Mat image);
 
